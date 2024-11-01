4b7dfc0b9332ea04352a5fc26f1b12d0d82fb4b0
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2024-11-01 14:53:39 -0300

    Finalizando banco
    Finalizando banco e inserindo as primeiras informações

--- banco.sql
+++ banco.sql
@@ -16,6 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `avaliacao`
+--
+
+DROP TABLE IF EXISTS `avaliacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliacao` (
+  `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nota` int(11) NOT NULL,
+  `comentario` varchar(600) DEFAULT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id_avaliacao`),
+  KEY `fk_usuario3` (`id_usuario`),
+  KEY `fk_produto3` (`id_produto`),
+  CONSTRAINT `fk_produto3` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`),
+  CONSTRAINT `fk_usuario3` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avaliacao`
+--
+
+LOCK TABLES `avaliacao` WRITE;
+/*!40000 ALTER TABLE `avaliacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avaliacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categoria`
 --
 
@@ -27,7 +58,7 @@ CREATE TABLE `categoria` (
   `nome` varchar(100) NOT NULL,
   `descricao` varchar(380) NOT NULL,
   PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +67,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Colar','Gargantilha,escapulário,colares,correntes, pendentes,colar de ouro, colar de prata,colar com ouro rose'),(2,'Anel','Anel de prata, Anel de ouro, Anel de diamante, Anel solitário,Anel de ouro Branco, Anel de ouro rose'),(3,'Brincos','Brincos de ouro,Brincos de prata,Brincos de argola de ouro,Brincos de ouro rose,Brincos de pedra,Brinco de argola dois tons'),(4,'Braceletes','Bracelete de ouro, Bracelete rigido de prata,Bracelete maleavel de prata,bracelete em ouro rose,Bracelete em dois tons'),(5,'Aliança','Aliança de ouro, Aliança de ouro com diamantes, Aliança de prata, Aliança de ouro branco,Aliança de ouro rose, Aliança de diamantes,Aliança banhada'),(6,'pingente','Pingente de prata, Pingente de ouro, Pingente de ouro branco,Pingente de ouro rose, Pingente de diamante');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,6 +95,97 @@ CREATE TABLE `endereco` (
 LOCK TABLES `endereco` WRITE;
 /*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
 /*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itens_pedido`
+--
+
+DROP TABLE IF EXISTS `itens_pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itens_pedido` (
+  `id_item` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pedido` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `preco` float NOT NULL,
+  PRIMARY KEY (`id_item`),
+  KEY `fk_pedido` (`id_pedido`),
+  KEY `fk_produto` (`id_produto`),
+  CONSTRAINT `fk_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`),
+  CONSTRAINT `fk_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itens_pedido`
+--
+
+LOCK TABLES `itens_pedido` WRITE;
+/*!40000 ALTER TABLE `itens_pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `itens_pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log_precos`
+--
+
+DROP TABLE IF EXISTS `log_precos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `log_precos` (
+  `id_log_precos` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `valor_anterior` float NOT NULL,
+  `valor_posterior` float NOT NULL,
+  PRIMARY KEY (`id_log_precos`),
+  KEY `fk_user` (`id_usuario`),
+  KEY `fk_prod` (`id_produto`),
+  CONSTRAINT `fk_prod` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log_precos`
+--
+
+LOCK TABLES `log_precos` WRITE;
+/*!40000 ALTER TABLE `log_precos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log_precos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `id_status` int(11) NOT NULL,
+  `total` float NOT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `fk_usuario1` (`id_usuario`),
+  KEY `fk_status1` (`id_status`),
+  CONSTRAINT `fk_status1` FOREIGN KEY (`id_status`) REFERENCES `status` (`id_status`),
+  CONSTRAINT `fk_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -94,6 +217,30 @@ CREATE TABLE `produto` (
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `id_status` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_status`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Pendente'),(2,'A pagar'),(3,'Preparando'),(4,'Cancelado'),(5,'A caminho'),(6,'Avaliar'),(7,'Não recebido');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -139,4 +286,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-29 14:31:37
+-- Dump completed on 2024-11-01 14:49:26
